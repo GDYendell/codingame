f59349e515d64050bb770dbcb439f2f377de0e1e
Gary Yendell <gary.yendell@diamond.ac.uk>
2019-07-23 21:01:00 +0100

    Actual PID Solution WIP

--- MarsLander/MarsLander.cpp
+++ MarsLander/MarsLander.cpp
@@ -2,13 +2,43 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 /**
  * Save the Planet.
  * Use less Fossil Fuel.
  **/
  
+#define GRAVITY 3.711;
+#define PI 3.14159265
  
+typedef struct Demand {
+    int power;
+    int rotation;
+} Demand;
+
+
+Demand calculateDemand(int currentX, int currentY, int targetX, int targetY, float kP) {
+    int powerX, powerY, demandRotation, demandPower, errorX, errorY, rotation, power;
+
+    errorX = currentX - targetX;
+    errorY = currentY - targetY;
+
+    powerX = errorX * kP;
+    powerY = GRAVITY + errorY * kP;
+    
+    float yOverX = powerY / powerX;
+
+    demandRotation = atan(powerY / powerX) * 180 / PI;
+    demandPower = sqrt(pow(powerX, 2) + pow(powerY, 2));
+
+    if (demandPower > 4) {demandPower = 4;}
+
+    std::cerr << "powerX: " << powerX << ", powerY: " << powerY << std::endl;
+    std::cerr << "demandRotation: " << demandRotation << ", demandPower: " << demandPower << std::endl;
+
+    return Demand{demandPower, demandRotation};
+}
 
 int main()
 {
@@ -20,16 +50,18 @@ int main()
     int R; // the rotation angle in degrees (-90 to 90).
     int P; // the thrust power (0 to 4).
 
+    int landX; // X coordinate of a surface point. (0 to 6999)
+    int landY; // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+    int landingZoneLeft, landingZoneRight, landingZoneCentre, landingZoneHeight;
+
     int direction;
+
+    Demand nextDemand;
 
     int N; // the number of points used to draw the surface of Mars.
     std::cin >> N; std::cin.ignore();
-    
     std::vector<int> landscapeX(N), landscapeY(N);
-    int landingZoneLeft, landingZoneRight, landingZoneHeight;
     for (int i = 0; i < N; i++) {
-        int landX; // X coordinate of a surface point. (0 to 6999)
-        int landY; // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
         std::cin >> landX >> landY; std::cin.ignore();
         landscapeX[i] = landX;
         landscapeY[i] = landY;
@@ -39,6 +71,7 @@ int main()
         if (landY == landscapeY[i - 1]) {
             landingZoneLeft = landX;
             landingZoneRight = landscapeX[i - 1];
+            landingZoneCentre = abs(landingZoneLeft - landingZoneRight) / 2;
             landingZoneHeight = landY;
             std::cerr << " <-- Landing Zone";
         }
@@ -50,20 +83,9 @@ int main()
         std::cin >> X >> Y >> HS >> VS >> F >> R >> P; std::cin.ignore();
 
         std::cerr << "Y: " << Y << std::endl;
-        
-        direction = HS/abs(HS);
 
-        if (Y < landingZoneHeight + 300) {
-            R = 0;
-            P = 4;
-        } else if (500 < Y && Y < 1750) {
-            R = direction * 20;
-            P = 4;
-        } else {
-            R = - direction * 20;
-            P = 3;
-        }
+        nextDemand = calculateDemand(X, Y, landingZoneCentre, landingZoneHeight, 0.005);
 
-        std::cout << R << " " << P << std::endl;
+        std::cout << nextDemand.rotation << " " << nextDemand.power << std::endl;
     }
 }
